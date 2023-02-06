18f152803b3805e4a57fe01c8c24369d94d70ac6
Dmitry <banindm@mail.ru>
2023-02-06 18:09:14 +0530

    added favicon & fix code

--- js/team-persons-elements.js
+++ js/team-persons-elements.js
@@ -1,39 +1,30 @@
 import { persons } from './data.js';
 
-const teamsElement = document.querySelector('.teams');
+const teamsBoxElement = document.querySelector('.teams__box');
 
-function getListContent(personsTeam) {
-  let fragment = new DocumentFragment();
-
-  personsTeam.forEach((person) => {
-    const { src, name, role } = person;
-    let li = document.createElement('li');
-    li.className = 'team';
-    li.insertAdjacentHTML(
-      'afterbegin',
-      `<a href="#" class="team__person-info">
-                  <div class="team__photo-box">
-                    <img
-                      class="team__photo"
-                      src=${src}
-                      alt="photo"
-                    />
-                  </div>
-                  <div class="team__desc-box">
-                    <div class="team__desc">
-                      <h2 class="team__name">${name}</h2>
-                      <p class="team__role">
-                        ${role}
-                      </p>
-                    </div>
-                  </div>
-                </a>
-              `
-    );
-    fragment.append(li);
-  });
-
-  return fragment;
-}
-
-teamsElement.append(getListContent(persons));
+teamsBoxElement.innerHTML = `<ul class="teams">
+    ${persons
+      .map((person) => {
+        let { src, name, role } = person;
+        return `<li class="team">
+      <a href="#" class="team__person-info">
+        <div class="team__photo-box">
+          <img
+            class="team__photo"
+            src=${src}
+            alt="photo"
+          />
+        </div>
+        <div class="team__desc-box">
+          <div class="team__desc">
+            <h2 class="team__name">${name}</h2>
+            <p class="team__role">
+              ${role}
+            </p>
+          </div>
+        </div>
+      </a>
+      </li>`;
+      })
+      .join('')}
+    </ul>`;
